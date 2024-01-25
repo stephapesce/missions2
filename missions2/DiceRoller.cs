786eb217a98b9e304ca19fb6817e5f3495051866
stephaniepesce <stephaniepesce@STEPHANIEPE95FD>
2024-01-24 23:44:26 -0700

    Add project files.

---
+++ missions2/DiceRoller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DiceRoller
+{
+    private Random random = new Random();
+
+    public int[] RollDice(int numRolls)
+    {
+        int[] results = new int[11]; 
+
+        for (int i = 0; i < numRolls; i++)
+        {
+            int rollOne = random.Next(1, 7);
+            int rollTwo = random.Next(1, 7);
+            int sum = rollOne + rollTwo;
+            results[sum - 2]++; 
+        }
+
+        return results;
+    }
+}
---
+++ missions2/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("How many dice rolls would you like to simulate? ");
+        int numRolls = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {numRolls}.\n");
+
+        DiceRoller diceRoller = new DiceRoller();
+        int[] results = diceRoller.RollDice(numRolls);
+
+        for (int i = 0; i < results.Length; i++)
+        {
+            int number = i + 2; 
+            double percentage = (double)results[i] / numRolls * 100;
+            Console.Write($"{number}: ");
+
+            for (int Count = 0; Count < (int)percentage; Count++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine($" ({percentage:0.00}%)");
+        }
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
